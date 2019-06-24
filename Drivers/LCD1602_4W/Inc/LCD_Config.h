58dccf786ed97fe0159ad8c3b84f352a8ebab556
SuWeipeng <weipeng.su@outlook.com>
2019-06-24 23:45:28 +0800

    加入 LCD1602 驱动（ 4 条数据线）

---
+++ Drivers/LCD1602_4W/Inc/LCD_Config.h
@@ -0,0 +1,33 @@
+
+#ifndef LCD_CONFIG_H
+#define LCD_CONFIG_H
+
+#include "main.h"
+
+#define _LCD_USE_FREERTOS 0
+#define _LCD_COLS         16
+#define _LCD_ROWS         2
+
+#define _LCD_RS_PORT      LCD_RS_GPIO_Port
+#define _LCD_RS_PIN       LCD_RS_Pin
+
+#define _LCD_E_PORT       LCD_E_GPIO_Port
+#define _LCD_E_PIN        LCD_E_Pin
+
+#define _LCD_RW_PORT      LCD_RW_GPIO_Port
+#define _LCD_RW_PIN       LCD_RW_Pin
+
+#define _LCD_D4_PORT      LCD_D4_GPIO_Port
+#define _LCD_D4_PIN				LCD_D4_Pin
+
+#define _LCD_D5_PORT      LCD_D5_GPIO_Port
+#define _LCD_D5_PIN       LCD_D5_Pin
+
+#define _LCD_D6_PORT      LCD_D6_GPIO_Port
+#define _LCD_D6_PIN       LCD_D6_Pin
+
+#define _LCD_D7_PORT      LCD_D7_GPIO_Port
+#define _LCD_D7_PIN       LCD_D7_Pin
+
+#endif
+
